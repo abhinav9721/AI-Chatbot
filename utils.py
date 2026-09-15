de8a111e8754bf6bd0c4dc3286af6ae8f17056bd
Abhinav Tripathi <abhinavtripathi97sg@gmail.com>
2026-09-15 15:41:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
+
 from groq import Groq
 from dotenv import load_dotenv
+
 
 # ==========================
 # Load Environment
@@ -18,33 +20,22 @@ client = Groq(
 # ==========================
 
 def get_ai_response(messages):
-
     try:
-
         completion = client.chat.completions.create(
-
-            model="model="openai/gpt-oss-120b"",
-
+            model="openai/gpt-oss-120b",
             messages=messages,
-
             temperature=0.5,
-
             max_tokens=800,
-
             top_p=1,
-
             stream=False
-
         )
 
         return completion.choices[0].message.content
 
     except Exception as e:
-
         error = str(e)
 
         if "Request too large" in error:
-
             return (
                 "⚠️ The uploaded document is too large for the current AI model.\n\n"
                 "Please upload a smaller PDF (recommended under 20–25 pages) "
@@ -52,12 +43,10 @@ def get_ai_response(messages):
             )
 
         elif "rate_limit_exceeded" in error:
-
             return (
                 "⚠️ AI request limit reached.\n\n"
                 "Please wait a few seconds and try again."
             )
 
         else:
-
             return f"❌ Error: {error}"
